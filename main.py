eb9a14f8234a4b740ed08f67c3cf7822cb921b62
Ndk985 <118891786+Ndk985@users.noreply.github.com>
2025-09-02 13:38:13 +0300

    анотация типов

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
-import uvicorn
 from fastapi import FastAPI
+from typing import Optional
+
 
 app = FastAPI()
 
 
-@app.get('/')
-def read_root():
-    return {'Hello': 'FastAPI'}
+@app.get('/me')
+def hello_author():
+    return {'Hello': 'author'}
 
 
-if __name__ == '__main__':
-    uvicorn.run('main:app', reload=True)
+@app.get('/{name}')
+def greetings(
+        name: str,
+        surname: str,
+        age: Optional[int] = None,
+        is_staff: bool = False
+) -> dict[str, str]:
+    result = ' '.join([name, surname])
+    result = result.title()
+    if age is not None:
+        result += ', ' + str(age)
+    if is_staff:
+        result += ', сотрудник'
+    return {'Hello': result}
