bdf1a342a83c05fbe5c8c42a285d4ad900da88f0
Ndk985 <118891786+Ndk985@users.noreply.github.com>
2025-09-06 16:16:24 +0300

    рефакторинг

--- app/api/endpoints.py
+++ app/api/endpoints.py
@@ -1,14 +1,15 @@
-from fastapi import Body, FastAPI
+from fastapi import APIRouter, Body
 
-from schemas import Person
+from app.schemas.schemas import Person
 
-app = FastAPI()
+router = APIRouter()
 
 
-@app.post('/hello')
+@router.post('/hello')
 def greetings(
         person: Person = Body(
-            ..., examples=Person.Config.schema_extra['examples']
+            ...,
+            examples=Person.Config.schema_extra['examples']
         )
 ) -> dict[str, str]:
     if isinstance(person.surname, list):
@@ -16,6 +17,7 @@ def greetings(
     else:
         surnames = person.surname
     result = ' '.join([person.name, surnames])
+    result = result.title()
     if person.age is not None:
         result += ', ' + str(person.age)
     if person.education_level is not None:
